cbf4311a00d6931a56c93131eaaab312152007dd
Stella Mbithe <mbithestella0@gmail.com>
2022-07-18 23:59:03 +0300

    fixed the indentation error

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 print('Welcome To Your Password Generator')
 
-chars = 'abcdefghijkABCDEFGHIJKLWXYZ!@#$%&*(.,?0123456789'
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLWXYZ!@#$%&*(.,?0123456789'
 
 number = input('Amount of passwords to generate: ')
 number = int(number)
@@ -10,10 +10,10 @@ number = int(number)
 length = input('Input Your Password Length:')
 length = int(length)
 
-print('\nhere are your passwords:')
+print('\nHere are your passwords:')
 
 for pwd in range(number):
     passwords = ''
     for c in range(length):
         passwords += random.choice(chars)
-        print(passwords)
+    print(passwords)
